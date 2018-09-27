b6c9913969fc6663edcb5910cb7b37590df0c759
Shoe Kure <rswkure@gmail.com>
2018-09-27 09:27:02 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/new'
   root 'static_pages#home'
+  #get '/home',to: 'static_pages/home' No longer to need since already set root 'static_pages#home'
+
+  get '/help',to: 'static_pages#help',as:'helf'
+  #演習5.3.3.1 help=>helfルーティング への名前変更　link_to "Help",helf_path　となる。
+  get '/about',to: 'static_pages#about'
+  get '/contact',to: 'static_pages#contact'
+
+  get '/signup',to: 'users#new'
+  #ユーザー登録用URLを初期値の/users/newから変更する。
 end
